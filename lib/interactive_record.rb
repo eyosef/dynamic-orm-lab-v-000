d0faa2a52618c13959c2602cc1efb793764f89d4
Eden Mesfun <emesfun@gmail.com>
2018-03-15 14:58:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,16 +4,7 @@ require 'active_support/inflector'
 class InteractiveRecord
 
   def self.table_name
-    sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      album TEXT
-      )
-    SQL
-
-    DB[:conn].execute(sql)
-    DB[:conn].results_as_hash = true 
+    self.to_s.downcase.pluralize
   end
 
 end
