2f2add6bba31f611e7cd70b2274115344c00151d
Eden Mesfun <emesfun@gmail.com>
2018-03-15 15:00:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,9 +12,7 @@ class InteractiveRecord
 
     sql = "PRAGMA table_info('#{table_name}')"
 
-    DB[:conn].results_as_hash = true
-
-    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
 
     table_info = DB[:conn].execute(sql)
     column_names = []
